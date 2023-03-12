170faf37081bf26cb345400bb0919a318db536d8
bl1yk <119798535+bl1yk@users.noreply.github.com>
2023-03-12 17:42:19 +0300

    Add files via upload

---
+++ indprog3/eq/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include "eq2.h"
+using namespace std;
+int main(){
+    double a, b, c;
+    cin >> a >> b >> c;
+    eq2 exmpl(a, b, c);
+    exmpl.find_X();
+    cout << exmpl.find_Y(6);
+    return 0;
+}
