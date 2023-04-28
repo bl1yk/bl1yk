5b4af21dc23d94c765961f1a9c88504b08a8d82a
bl1yk <119798535+bl1yk@users.noreply.github.com>
2023-04-29 00:22:17 +0300

    Add files via upload

---
+++ indprog8(curs3_4)/main.cpp
@@ -0,0 +1,18 @@
+#include "type3_4.h"
+#include <iostream>
+
+int main() {
+
+
+	std::cout << "Введите коэффициент уравнения С для уравнения 3-го типа: "; int val; std::cin >> val;
+	std::cout << "Введите коэффициенты уравнения А и C для уравнения 4-го типа: "; int _A, _C; std::cin >> _A >> _C;
+
+	type3 first(val);
+	first.show();
+	first.get_answer();
+
+	type4 second(_A, _C);
+	second.show();
+	second.get_answer();
+
+}
