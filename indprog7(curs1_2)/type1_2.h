2b50c29ea8a2db01a9f791e419b8eff1515742d9
bl1yk <119798535+bl1yk@users.noreply.github.com>
2023-04-29 00:22:00 +0300

    Add files via upload

---
+++ indprog7(curs1_2)/type1_2.h
@@ -0,0 +1,23 @@
+
+#ifndef type1_2
+#define type1_2
+
+class Type1Equation {
+public:
+    void show();
+    void get_answer();
+};
+
+class Type2Equation {
+private:
+    double a;
+public:
+    Type2Equation(double a1);
+    void show();
+    void get_answer();
+};
+
+#endif
+
+
+
